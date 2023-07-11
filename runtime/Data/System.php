08427a8f4b0e54b22d4d125eb331eae485b173df
yydsxyh <137990950+yydsxyh@users.noreply.github.com>
2023-07-11 16:43:08 +0800

    Add files via upload

---
+++ runtime/Data/System.php
@@ -0,0 +1,16 @@
+<?php
+return array (
+  'id' => 1,
+  'name' => '兆鸿金融',
+  'url' => 'http://www.jizhanli.com/',
+  'title' => '兆鸿金融',
+  'key' => '兆鸿金融',
+  'des' => '兆鸿金融',
+  'bah' => '陕ICP备15008293号-3',
+  'copyright' => '2015-2020',
+  'ads' => '深圳市兆鸿金融',
+  'tel' => '18792402222',
+  'email' => '1109302222@qq.com',
+  'logo' => '',
+);
+?>
